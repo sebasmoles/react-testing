22c612ace41ca8c3401947d45125b423edc02969
Sebastian <roshfeltsemo@gmail.com>
2022-02-17 09:35:40 -0500

    integration tests example

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -2,27 +2,19 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
 
-// Run tests with: yarn test (in this project)
-// screen.getByRole("") is a good way to debug our tests;
-// Follow Arrange, Act, Assert pattern for unit testing
-// Try to test the contrary of whatever you want to test for debugging. For instance if you want to check if a button is enabled, also test if is disabled.
+// Run tests with: yarn test
+// Integration tests are about testing multiple units of code, which usually results in one larger test.
+// Integration testing is about realistic user flows. Think really how the user would use your application, and test that process.
+// Is common to have multiple assertions in this type of test.
+// In general, is better to have a few integration tests than hundreds of small and precise unit tests.
 
-test("on initial render, the pay button is disabled", async () => {
-  // Arrange
+test("if an amount and note is entered, the pay button becomes enabled, if not it remains disabled", async () => {
   render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
 
-  // Assert, no act needed in this particular test
   expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
-});
 
-test("if an amount and note is entered, the pay button becomes enabled", async () => {
-  // Arrange (which is basically rendering de component first)
-  render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
-
-  // Act (could be clicking, typing, any user event)
   userEvent.type(screen.getByPlaceholderText(/amount/i), "50");
   userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner");
 
-  // Assert
   expect(await screen.findByRole("button", { name: /pay/i })).toBeEnabled();
 });
